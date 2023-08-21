501a0ff04c228ec5748c29ddeeb9b813c4cd04c9
dimash-git <dimash9i@yandex.com>
2023-08-21 10:31:20 +0600

    react hook form adjusted for file upload

--- components/forms/constants.ts
+++ components/forms/constants.ts
@@ -1,4 +1,11 @@
 import * as z from "zod";
+const MAX_FILE_SIZE = 5000000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
 
 const addLinkFormSchema = z.object({
   title: z.string().min(2, {
@@ -11,7 +18,17 @@ const addLinkFormSchema = z.object({
 });
 
 const profileFormSchema = z.object({
-  image: z.instanceof(File),
+  image: z
+    .any()
+    .refine((file) => file?.name, "Image is required.")
+    .refine(
+      (file) => file?.size < MAX_FILE_SIZE,
+      `Max file size is ${MAX_FILE_SIZE / 1000000} MB.`
+    ) // this should be greater than or equals (>=) not less that or equals (<=)
+    .refine(
+      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+      "Only .jpg, .jpeg, .png and .webp files are accepted."
+    ),
 });
 
 export { addLinkFormSchema, profileFormSchema };
--- components/forms/profile-form.tsx
+++ components/forms/profile-form.tsx
@@ -1,27 +1,49 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ImageUpload from "../image-upload";
 import { Button } from "../ui/button";
 import axios from "axios";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { profileFormSchema as formSchema } from "./constants";
+import { Input } from "../ui/input";
 
 const ProfileForm = () => {
-  const [image, setImage] = useState<File | null>(null);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      image: {} as File,
+    },
+  });
 
-  const onClick = async () => {
+  const { isLoading } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const { image } = values;
+    console.log(image);
+
     if (!image) return;
     try {
       // console.log(image);
-      const formData = new FormData();
-      formData.append("image", image);
-      formData.append("image_name", image.name);
-
-      const { data } = await axios.post("/api/profile/picture", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data", // Set the content type for file upload
-        },
-      });
-      console.log(data);
+      // const formData = new FormData();
+      // formData.append("image", image);
+      // formData.append("image_name", image.name);
+      // const { data } = await axios.post("/api/profile/picture", formData, {
+      //   headers: {
+      //     "Content-Type": "multipart/form-data", // Set the content type for file upload
+      //   },
+      // });
+      // console.log(data);
     } catch (error) {
       console.log("PROFILE_IMAGE_UPLOAD_ERROR", error);
     }
@@ -29,10 +51,33 @@ const ProfileForm = () => {
 
   return (
     <>
-      <ImageUpload {...{ image, setImage }} />
-      <Button className="w-full mt-4" onClick={onClick}>
-        Update Profile Image
-      </Button>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field: { value, ...field } }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <Input
+                    type="file"
+                    {...field}
+                    onChange={(e) => {
+                      if (!e.target.files) return;
+                      field.onChange(e.target.files[0]);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button disabled={isLoading} className="w-full mt-4">
+            Update Profile Image
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
