fd5790b268346f2256c1f0405ee1de136dab67ce
dimash-git <dimash9i@yandex.com>
2023-08-21 19:06:22 +0600

    image preview

--- app/api/profile/picture/route.ts
+++ app/api/profile/picture/route.ts
@@ -1,7 +1,6 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { decode } from "base64-arraybuffer";
 
 export async function POST(req: Request) {
   const supabase = createRouteHandlerClient({ cookies });
@@ -14,23 +13,19 @@ export async function POST(req: Request) {
     return new Response("Unauthorized", { status: 401 });
   }
 
-  // const { image }: { image: File } = await req.json();
-
   const formData = await req.formData();
-  const image = formData.get("image");
+  const imageBlob = formData.get("image_blob");
   const imageName = formData.get("image_name");
 
-  if (!image) {
+  if (!imageBlob) {
     return new Response("Image not provided", { status: 500 });
   }
-
-  console.log("image,", image.toString());
 
   const { user } = session;
 
   const { data, error } = await supabase.storage
     .from("profile_pictures")
-    .upload(`${user.id}/${imageName}`, image, {
+    .upload(`${user.id}/${imageName}`, imageBlob, {
       upsert: true,
     });
 
--- components/forms/profile-form.tsx
+++ components/forms/profile-form.tsx
@@ -17,6 +17,7 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { profileFormSchema as formSchema } from "./constants";
 import { Input } from "../ui/input";
+import Image from "next/image";
 
 const ProfileForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -26,24 +27,27 @@ const ProfileForm = () => {
     },
   });
 
+  const { watch } = form;
+  const image = watch("image");
+  console.log("image: ", image);
+
   const { isLoading } = form.formState;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     const { image } = values;
-    console.log(image);
-
-    if (!image) return;
     try {
-      // console.log(image);
-      // const formData = new FormData();
-      // formData.append("image", image);
-      // formData.append("image_name", image.name);
-      // const { data } = await axios.post("/api/profile/picture", formData, {
-      //   headers: {
-      //     "Content-Type": "multipart/form-data", // Set the content type for file upload
-      //   },
-      // });
-      // console.log(data);
+      const formData = new FormData();
+
+      formData.append("image_blob", image);
+      formData.append("image_name", image.name);
+
+      const { data } = await axios.post("/api/profile/picture", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data", // Set the content type for file upload
+        },
+      });
+
+      console.log(data);
     } catch (error) {
       console.log("PROFILE_IMAGE_UPLOAD_ERROR", error);
     }
@@ -73,6 +77,18 @@ const ProfileForm = () => {
               </FormItem>
             )}
           />
+          {image?.name && (
+            <Image
+              src={URL.createObjectURL(image)}
+              width={300}
+              height={300}
+              alt="Uploaded Image"
+              className="w-full h-32 object-cover hover:grayscale transition-all duration-150 cursor-not-allowed mt-4 rounded-sm"
+              onClick={(e) => {
+                form.resetField("image");
+              }}
+            />
+          )}
           <Button disabled={isLoading} className="w-full mt-4">
             Update Profile Image
           </Button>
